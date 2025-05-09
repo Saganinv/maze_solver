7f45a6a276c7329cd94e8a2404447e39f69fb896
Saganinv <saganinvestments@gmail.com>
2025-05-10 08:33:59 +1200

    cell functionality added

--- graphics.py
+++ graphics.py
@@ -3,22 +3,43 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("Maze Solver")
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
-        self.__canvas.pack(fill=BOTH, expand=1)
-        self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.root = Tk()
+        self.root.title("Maze Solver")
+        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
+        self.root.update_idletasks()
+        self.root.update()
 
     def wait_for_close(self):
-        self.__running = True
-        while self.__running:
+        self.running = True
+        while self.running:
             self.redraw()
         print("window closed...")
 
     def close(self):
-        self.__running = False
+        self.running = False
+
+    def draw_line(self, line, fill_colour="black"):
+        line.draw(self.canvas, fill_colour)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point_one, point_two):
+        self.point_one = point_one
+        self.point_two = point_two
+
+    def draw(self, canvas, fill_colour):
+        canvas.create_line(
+        self.point_one.x, self.point_one.y,
+        self.point_two.x, self.point_two.y,
+        fill=fill_colour, width=2
+        )
